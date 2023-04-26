1a195dc5b4592c648964ba770f556ebe34d3e7da
Dyes Jr <donjnr95@gmail.com>
2023-04-26 18:58:05 +0300

    Added responsiveness to the dice button

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const adviceId = document.querySelector('a#dviceId')
+const adviceId = document.querySelector('#adviceId')
 const adviceText = document.querySelector('#advicetext')
 const btn = document.querySelector('#btn')
 
-fetch('https://api.adviceslip.com/advice').then(res => 
+function getAdvice() {
+    fetch('https://api.adviceslip.com/advice').then(res => 
         { 
-            // return res.json()
-            console.log(res.json())
+            return res.json()
+            // console.log(res.json())
+        }).then(adviceData => {
+            const adviceNum = adviceData.slip.id
+            const advice = adviceData.slip.advice
+
+            adviceId.textContent = adviceNum;
+            adviceText.innerHTML = `<p>${advice}</p>`;
+
+        }).catch(error => {
+            console.log(error)
         })
+}
+
+btn.addEventListener('click', getAdvice)
+
+window.onload = () => {
+    getAdvice()
+}
+
